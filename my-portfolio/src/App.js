7dbcffdd58664fbbac66a82f832869a9a08ff0c7
jessef <1319957+jfrausto@users.noreply.github.com>
2021-01-26 21:40:51 -0800

    create second grid for main content

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -35,6 +35,13 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
+
+        {/* 
+            WILL HOLD NAME INTRO
+            TYPOGRAPHY
+            COLOR MODE SWITCHER
+            
+        */}
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
@@ -44,7 +51,46 @@ function App() {
             </Text>
             <Button>yooo</Button>
             <Text color="brand.700">hi i should be red</Text>
+          </VStack>
+        </Grid>
 
+        {/* 
+            PLACE HOLDING TEXT EVERYWHERE rn
+            these two grids provide 100% window height rn
+            will contain side navigation bar with
+            - about me
+            - links and screenshots to projects
+            - contact 
+        */}
+        <Grid minH="100vh" p={3}>
+          <VStack spacing={0}>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
+            <Text color="brand.300">
+              Portfolio coming soon...!
+            </Text>
           </VStack>
         </Grid>
       </Box>
