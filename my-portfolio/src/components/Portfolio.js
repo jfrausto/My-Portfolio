56b8f05006bcd3e281ec3915adfc21f5f272efa6
jessef <1319957+jfrausto@users.noreply.github.com>
2021-02-08 02:36:34 -0800

    add tags to portfolio

--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -7,8 +7,10 @@ import {
   Box,
   useColorModeValue,
   VStack,
+  HStack,
   Link,
-  useColorMode
+  Tag,
+  Badge
 } from '@chakra-ui/react'
 import React from 'react';
 import VibeeMov from './videos/Vibee_TAB_demo.mp4';
@@ -22,7 +24,7 @@ import BurgerDBMov from './videos/Burger_DB_TAB_demo.mp4';
 export default function Portfolio() {
   return (
     <div>
-      <Box as={Heading} size="xl" marginBottom="2rem" 
+      <Box as={Heading} size="xl" marginBottom="0.5rem" 
         textDecoration="underline crimson"
         color={useColorModeValue("brand.900", "brand.100")}
         marginTop={1}
@@ -44,6 +46,36 @@ export default function Portfolio() {
                   <video autoplay="true" loop="true">
                     <source src={VibeeMov} type="video/mp4" />
                   </video>
+                  <HStack spacing={1}>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="blue"
+                    >
+                      React
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="red"
+                    >
+                      Express
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="green"
+                    >
+                      MongoDB
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="yellow"
+                    >
+                      Socket.io
+                    </Tag>
+                  </HStack>
                 </VStack>
             </Center>
           </WrapItem>
@@ -58,6 +90,29 @@ export default function Portfolio() {
                 <video autoplay="true" loop="true">
                   <source src={PubCrawlMov} type="video/mp4" />
                 </video>
+                <HStack spacing={1}>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="purple"
+                    >
+                      Materialize
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="red"
+                    >
+                      Express
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="orange"
+                    >
+                      jQuery
+                    </Tag>
+                  </HStack>
               </VStack>
             </Center>
           </WrapItem>
@@ -72,6 +127,29 @@ export default function Portfolio() {
                 <video autoplay="true" loop="true">
                   <source src={LearnSquadMov} type="video/mp4" />
                 </video>
+                <HStack spacing={1}>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="green"
+                    >
+                      SQL
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="red"
+                    >
+                      Express
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="purple"
+                    >
+                      Bootstrap
+                    </Tag>
+                  </HStack>
               </VStack>
             </Center>
           </WrapItem>
@@ -86,6 +164,29 @@ export default function Portfolio() {
                 <video autoplay="true" loop="true">
                   <source src={BurgerDBMov} type="video/mp4" />
                 </video>
+                <HStack spacing={1}>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="red"
+                    >
+                      Express
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="green"
+                    >
+                      SQL
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="purple"
+                    >
+                      Bootstrap
+                    </Tag>
+                  </HStack>
               </VStack>
             </Center>
           </WrapItem>
@@ -100,6 +201,29 @@ export default function Portfolio() {
                 <video autoplay="true" loop="true">
                   <source src={BudgetTrackerMov} type="video/mp4" />
                 </video>
+                <HStack spacing={1}>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="red"
+                    >
+                      Express
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="green"
+                    >
+                      SQL
+                    </Tag>
+                    <Tag 
+                      size="sm"
+                      variant="subtle"
+                      colorScheme="yellow"
+                    >
+                      Webpack
+                    </Tag>
+                  </HStack>
               </VStack>
             </Center>
           </WrapItem>
