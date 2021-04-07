b425a3769596b0ee4b98c96cdb4490237d7e884f
jessef <1319957+jfrausto@users.noreply.github.com>
2021-04-06 17:59:35 -0700

    add animated arrows, bg gradient to name

--- src/components/MainHeading.js
+++ src/components/MainHeading.js
@@ -4,11 +4,27 @@ import {
   VStack,
   Heading,
   Text,
+  chakra
  } from '@chakra-ui/react';
-// import FloatingArrows from './FloatingArrows';
+ import { useSpring, animated, config} from 'react-spring';
 
+// interface animated react-spring with chakra Text component
+const AnimatedText = animated(Text);
 
 export default function MainHeading() {
+  const props = useSpring({
+    to: async (next, cancel) => {
+      await next({ transform: 'translateY(20px)'});
+      await next({ transform: 'translateY(0px)'});
+      await next({ transform: 'translateY(20px)'});
+      await next({ transform: 'translateY(0px)'});
+      await next({ transform: 'translateY(20px)'});
+      await next({ transform: 'translateY(0px)'});
+      await next({ transform: 'translateY(20px)'});
+    },
+    from: {transform: 'translateY(0px)'},
+    config: config.wobbly
+  });
   return (
     <>
       <VStack spacing={8}>
@@ -21,17 +37,25 @@ export default function MainHeading() {
               letterSpacing="tighter"
               zIndex="overlay"
             >
-              Hi, my name is Jesse Frausto. I'm a full-stack web developer.
+              Hi, my name is 
+                <chakra.span
+                  bgGradient={`linear(to-l, ${useColorModeValue("brand.500", "brand.700")}, ${useColorModeValue("brand.700", "brand.500")})`}
+                  bgClip="text"
+                > Jesse Frausto
+                </chakra.span>. 
+              I'm a full-stack web developer.
             </Heading>
-            <Text 
+            <AnimatedText 
               p={2}
               borderRadius="lg"
               bg={useColorModeValue("brand.900", "transparent")}
               color={useColorModeValue("brand.300", "brand.700")} 
-              fontWeight="black" 
+              fontWeight="black"
+              position="relative"
+              style={props}
             >
               V V V V V V V V V V 
-            </Text>
+            </AnimatedText>
           </VStack>
     </>
   )
